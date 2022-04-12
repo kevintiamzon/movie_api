ebeeb1206ec1c85b55a5e084a91dc3daa811fa6c
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-04-12 12:45:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/users',
     ], (req, res) => {
 
         // check the validation object for errors
-        let errors = validationResults(req);
+        let errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
